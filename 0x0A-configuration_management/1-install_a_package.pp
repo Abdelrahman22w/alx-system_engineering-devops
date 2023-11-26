c8ac635f812110342548491c560904bcda991c1f
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-26 12:26:28 +0200

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+# install Werkzueg version 2.0.0
+package { 'Werkzueg':
+  ensure   => '0.16.1',
   provider => 'pip3'
 }
