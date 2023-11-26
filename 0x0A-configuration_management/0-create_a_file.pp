1687aa9009cc53699727e57eae4c99039ae58ace
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-26 12:23:53 +0200

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# create a file 
+# creates a file in /tmp
 
-file { '/temp/school:'
+file { '/tmp/school':
   content =>'I love Puppet',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love Puppet'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
