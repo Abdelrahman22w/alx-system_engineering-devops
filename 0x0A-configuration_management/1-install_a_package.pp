e97a9315eb316cca6e5477fd6fa525aeb8a3fb45
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-26 12:46:30 +0200

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,18 @@
-# Install puppet-lint
-package { 'puppet-lint':
+# Define the package resource for Python 3
+package { 'python3':
+  ensure => 'latest',
+}
+
+# Define the package resource for Flask 2.1.0 using pip3
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip',
+  require  => Package['python3'], # Ensure Python is installed first
+}
+
+# Define the package resource for Werkzeug 2.1.1 using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['Flask'], # Ensure Flask is installed first
 }
