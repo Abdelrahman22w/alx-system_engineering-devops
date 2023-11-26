16e00db4eb571aaf50f368bc9a3e8a5bd6532389
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-26 12:39:52 +0200

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,22 @@ package { 'puppet-lint':
   ensure   => '2.1.1',
   provider => 'gem',
 }
+
+# Install Python 3.8.10
+package { 'python':
+  ensure => '3.8.10',
+}
+
+# Install Flask 2.1.0
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python'],
+}
+
+# Install Werkzeug 2.1.1
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['Flask'],
+}
